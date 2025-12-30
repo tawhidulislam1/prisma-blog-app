6abb692d419599e1ab49134c7abd007c4d527fe2
unknown <designwithtawhid@gmail.com>
2025-12-30 20:14:37 +0600

    udpated schema

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express, { Application } from "express";
 import { PostRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+
 app.use(express.json());
 
 app.use("/post", PostRouter);
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,28 @@ import { prisma } from "./prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
+    provider: "postgresql",
   }),
+  trustedOrigins: [process.env.AUTH_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
 });
