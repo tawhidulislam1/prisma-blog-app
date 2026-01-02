7149e41f85851f31f1430b4352b3ac7d12032329
unknown <designwithtawhid@gmail.com>
2026-01-02 23:43:52 +0600

    seedAdmin upload

---
+++ src/script/seedAdmin.ts
@@ -0,0 +1,39 @@
+import { error } from "node:console";
+import { prisma } from "../lib/prisma";
+import { USERROLE } from "../middlewere/auth";
+import { METHODS } from "node:http";
+
+async function seedAdmin() {
+  try {
+    const adminData = {
+      name: process.env.ADMIN_NAME,
+      email: process.env.ADMIN_EMAIL,
+      role: USERROLE.ADMIN,
+      password: process.env.ADMIN_PASSWORD,
+    };
+    //check user exist on db or not
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: adminData.email as string,
+      },
+    });
+    if (existingUser) {
+      throw new Error("User Alreadt Exists");
+    }
+    const signUpAdmin = await fetch(
+      "http://localhost:5000/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify(adminData),
+      }
+    );
+    console.log(signUpAdmin);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+seedAdmin()
